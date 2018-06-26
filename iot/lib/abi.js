aeb268b028550a8cc4e87aa669184e5af66633eb
Michal Takac <takacmichal92@gmail.com>
2018-06-26 15:01:08 +0200

    update ABI

--- iot/lib/abi.js
+++ iot/lib/abi.js
@@ -1,79 +1,12 @@
 const ABI = [
   {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: false,
-        name: "allower",
-        type: "address"
-      },
-      {
-        indexed: false,
-        name: "allowedAddress",
-        type: "address"
-      }
-    ],
-    name: "AccessAllowed",
-    type: "event"
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: false,
-        name: "revoker",
-        type: "address"
-      },
-      {
-        indexed: false,
-        name: "revokedAddress",
-        type: "address"
-      }
-    ],
-    name: "AccessRevoked",
-    type: "event"
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "incomingPerson",
-        type: "address"
-      }
-    ],
-    name: "giveAccess",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "incomingPerson",
-        type: "address"
-      }
-    ],
-    name: "revokeAccess",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function"
-  },
-  {
     constant: true,
-    inputs: [
-      {
-        name: "",
-        type: "address"
-      }
-    ],
-    name: "accessAllowance",
+    inputs: [],
+    name: "APP_ADDR_NAMESPACE",
     outputs: [
       {
         name: "",
-        type: "bool"
+        type: "bytes32"
       }
     ],
     payable: false,
@@ -82,13 +15,8 @@ const ABI = [
   },
   {
     constant: true,
-    inputs: [
-      {
-        name: "",
-        type: "uint256"
-      }
-    ],
-    name: "allowedAddresses",
+    inputs: [],
+    name: "getRecoveryVault",
     outputs: [
       {
         name: "",
@@ -121,11 +49,133 @@ const ABI = [
   {
     constant: true,
     inputs: [],
-    name: "getAllAllowedAddresses",
+    name: "EVMSCRIPT_REGISTRY_APP_ID",
     outputs: [
       {
         name: "",
-        type: "address[]"
+        type: "bytes32"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: "",
+        type: "uint256"
+      }
+    ],
+    name: "allowedAddresses",
+    outputs: [
+      {
+        name: "",
+        type: "address"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: "token",
+        type: "address"
+      }
+    ],
+    name: "allowRecoverability",
+    outputs: [
+      {
+        name: "",
+        type: "bool"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "appId",
+    outputs: [
+      {
+        name: "",
+        type: "bytes32"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "ETH",
+    outputs: [
+      {
+        name: "",
+        type: "address"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: "incomingPerson",
+        type: "address"
+      }
+    ],
+    name: "revokeAccess",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "REVOKE_ROLE",
+    outputs: [
+      {
+        name: "",
+        type: "bytes32"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getInitializationBlock",
+    outputs: [
+      {
+        name: "",
+        type: "uint256"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "EVMSCRIPT_REGISTRY_APP",
+    outputs: [
+      {
+        name: "",
+        type: "bytes32"
       }
     ],
     payable: false,
@@ -145,6 +195,175 @@ const ABI = [
     payable: false,
     stateMutability: "view",
     type: "function"
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: "_token",
+        type: "address"
+      }
+    ],
+    name: "transferToVault",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: "_sender",
+        type: "address"
+      },
+      {
+        name: "_role",
+        type: "bytes32"
+      },
+      {
+        name: "params",
+        type: "uint256[]"
+      }
+    ],
+    name: "canPerform",
+    outputs: [
+      {
+        name: "",
+        type: "bool"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: "incomingPerson",
+        type: "address"
+      }
+    ],
+    name: "giveAccess",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "kernel",
+    outputs: [
+      {
+        name: "",
+        type: "address"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getAllAllowedAddresses",
+    outputs: [
+      {
+        name: "",
+        type: "address[]"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "ALLOW_ROLE",
+    outputs: [
+      {
+        name: "",
+        type: "bytes32"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: "",
+        type: "address"
+      }
+    ],
+    name: "accessAllowance",
+    outputs: [
+      {
+        name: "",
+        type: "bool"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: "_script",
+        type: "bytes"
+      }
+    ],
+    name: "getExecutor",
+    outputs: [
+      {
+        name: "",
+        type: "address"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        name: "allower",
+        type: "address"
+      },
+      {
+        indexed: false,
+        name: "allowedAddress",
+        type: "address"
+      }
+    ],
+    name: "AccessAllowed",
+    type: "event"
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        name: "revoker",
+        type: "address"
+      },
+      {
+        indexed: false,
+        name: "revokedAddress",
+        type: "address"
+      }
+    ],
+    name: "AccessRevoked",
+    type: "event"
   }
 ];
 
