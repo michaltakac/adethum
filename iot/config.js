b9dd8148ac417860fa0b2cd67f8f8bab913c7629
Michal Takac <takacmichal92@gmail.com>
2018-07-15 00:37:48 +0200

    fix: comma

--- iot/config.js
+++ iot/config.js
@@ -1,6 +1,6 @@
 const config = {
   CONTRACT_ADDR: "0x9BD33a68B9d7602110A51097101f5C1d2e4FB7d3",
-  NODE_ADDR: "http://192.168.1.4" // set URL that is not changing often
+  NODE_ADDR: "http://192.168.1.4", // set URL that is not changing often
   NODE_PORT: "8545"
 };
 
